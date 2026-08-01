484c6ae22dc4271359ec99a1ac99d53199e89ee6
VolodAs <vladimiraseev89@gmail.com>
2026-08-01 20:28:14 +0200

    add api baseurl

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -6,4 +6,9 @@ export const nextServer = axios.create({
   withCredentials: true,
 });
 
+export const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  withCredentials: true,
+});
+
 export type ApiError = AxiosError<{ error: string }>;
